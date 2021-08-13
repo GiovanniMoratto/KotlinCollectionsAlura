f45d7514c40d61bac37c846062dd9e09e19510ac
Giovanni Moratto <morattotrabalho@gmail.com>
2021-08-12 23:46:24 -0300

    v2.5

---
+++ src/main/kotlin/br/com/alura/list/Prateleira.kt
@@ -0,0 +1,20 @@
+package br.com.alura.list
+
+/**
+ *@Author giovanni.moratto
+ */
+
+data class Prateleira(
+    val genero: String,
+    val livros: MutableList<Livro>
+) {
+    fun organizarPorAutor(): MutableList<Livro> {
+        livros.sortBy { it.autor }
+        return livros
+    }
+
+    fun organizarPorAnoPublicacao(): MutableList<Livro> {
+        livros.sortBy { it.anoPublicacao }
+        return livros
+    }
+}
